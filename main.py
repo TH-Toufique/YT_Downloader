b8fbaf6821c706f236c9f02d25dc950d40c74bbf
TH-Toufique <mrripp3r96@gmail.com>
2024-11-06 23:50:50 +0600

    Determine OS and set download path

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 import time
 import subprocess
 import platform
-
+import yt_dlp
+from colorama import Fore, Style, init
 # Banner function
 def WriteText(z):
     for e in z + '\n':
@@ -34,5 +35,20 @@ def install_and_import(package):
 #Check and install missing package
 install_and_import("yt_dlp")
 install_and_import("colorama")
+
+# Function to determine OS and set download path
+def get_download_path():
+    # Check for Termux
+    if "com.termux" in os.getenv("PREFIX", ""):
+        return check_storage_permission()
+    # Check for Linux distros based on their IDs in os-release
+    elif os.path.isfile("/etc/os-release"):
+        with open("/etc/os-release") as f:
+            os_info = f.read()
+            if "debian" in os_info or "ubuntu" in os_info or "arch" in os_info or "fedora" in os_info:
+                return os.path.expanduser("~/Downloads/")
+    # Default to Downloads in case of other or unknown OS
+    return os.path.expanduser("~/Downloads/")
+
 
 
