17a3ef8d26f7098dcceceb2301faddb3ca4f335e
TH-Toufique <mrripp3r96@gmail.com>
2024-11-06 23:43:16 +0600

    Check and install missing modules

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 import sys
 import time
+import subprocess
+import platform
 
 # Banner function
 def WriteText(z):
@@ -19,3 +21,18 @@ banner = """\033[096m
    ╚═╝      ╚═╝       ╚═════╝  ╚═════╝  ╚══╝╚══╝ ╚═╝  ╚═══╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚═════╝ ╚══════╝╚═╝  ╚═╝
                                                                                                             By Ancient Modder"""
 WriteText(banner)
+
+
+# Functions to check and intall missing modules
+def install_and_import(package):
+    try:
+        import(package)
+    except ImportError:
+        print(f"Installing missing modules: {package}")
+        subprocess.check_call([sys.executable, "-m", "pip", "install" ,package])
+        
+#Check and install missing package
+install_and_import("yt_dlp")
+install_and_import("colorama")
+
+
